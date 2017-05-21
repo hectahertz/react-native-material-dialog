65ff709f7d36cbd6bc6f4b4e8702f39807ae4c98
Hector Garcia <hectahertz@gmail.com>
2017-05-21 13:55:44 +0200

    upgraded the example project to react-native 0.44

--- example/android/app/src/main/java/com/materialdialog/MainApplication.java
+++ example/android/app/src/main/java/com/materialdialog/MainApplication.java
@@ -1,7 +1,6 @@
 package com.materialdialog;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
