81f33302255f0b9fd7d236cf476294574818592e
Kyle Roach <kroach.work@gmail.com>
2017-05-25 14:36:19 -0400

    Title prop on Multipicker and Singlepicker should be optional

--- src/MultiPickerMaterialDialog.js
+++ src/MultiPickerMaterialDialog.js
@@ -123,7 +123,7 @@ MultiPickerMaterialDialog.propTypes = {
   visible: PropTypes.bool.isRequired,
   items: PropTypes.arrayOf(PropTypes.object).isRequired,
   selectedItems: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string,
   titleColor: PropTypes.string,
   colorAccent: PropTypes.string,
   onCancel: PropTypes.func.isRequired,
